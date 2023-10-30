7e2a1c576501d09838bc910999a5e3f0787f6823
Neirotciv <tanacetifolia@gmail.com>
2023-10-30 15:18:55 +0100

    add goal to the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import React, { useState } from 'react';
 
 const sampleGoals = [
   "Faire les courses",
@@ -13,18 +14,43 @@ const sampleGoals = [
   "Faire un triathlon",
 ];
 
+function addGoal(goal) {
+  if (goal !== '') {
+    sampleGoals = [...sampleGoals, goal];
+  }
+}
+
 function App() {
+  const [inputValue, setInputValue] = useState();
+  const [goals, setGoals] = useState(sampleGoals);
+
+  const handleGoals = () => {
+    setGoals([...goals, inputValue]);
+    setInputValue('');
+  };
+
+  
+
   return (
     <>
       <div>
         <ul>
-          {sampleGoals.map((goal, index) => 
+          {goals.map((goal, index) => 
             <li key={index} className="">
                 {goal}
             </li>
           )}
         </ul>
       </div>
+
+      <input 
+        type="text"
+        placeholder="Ajouter un nouveau but"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      
+      <button onClick={handleGoals}>Ajouter</button>
     </>
   )
 }
