59b820c42917c9981c9c09228d775562b5d33c10
Neirotciv <tanacetifolia@gmail.com>
2023-10-30 16:19:38 +0100

    feat: remove goal from the list

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import './App.css'
 import React, { useState } from 'react';
 
 const sampleGoals = [
-  "Faire les courses",
-  "Aller à la salle de sport 3 fois par semaine",
-  "Monter à plus de 5000m d'altitude",
-  "Acheter mon premier appartement",
-  "Perdre 5 kgs",
-  "Gagner en productivité",
-  "Apprendre un nouveau langage",
-  "Faire une mission en freelance",
-  "Organiser un meetup autour de la tech",
-  "Faire un triathlon",
+  {id: 1, text: "Faire les courses"},
+  {id: 2, text: "Aller à la salle de sport 3 fois par semaine"},
+  {id: 3, text: "Monter à plus de 5000m d'altitude"},
+  {id: 4, text: "Acheter mon premier appartement"},
+  {id: 5, text: "Perdre 5 kgs"},
+  {id: 6, text: "Gagner en productivité"},
+  {id: 7, text: "Apprendre un nouveau langage"},
+  {id: 8, text: "Faire une mission en freelance"},
+  {id: 9, text: "Organiser un meetup autour de la tech"},
+  {id: 10, text: "Faire un triathlon"},
 ];
 
 function addGoal(goal) {
@@ -24,20 +24,38 @@ function App() {
   const [inputValue, setInputValue] = useState();
   const [goals, setGoals] = useState(sampleGoals);
 
-  const handleGoals = () => {
-    setGoals([...goals, inputValue]);
-    setInputValue('');
+  const handleAddGoal = () => {
+    if (inputValue !== '') {
+      const lastId = goals[goals.length-1].id;
+      const newGoal = {id: lastId + 1, text: inputValue};
+
+      setGoals([...goals, newGoal]);
+      setInputValue('');
+    }
   };
 
-  
+  const handleInputGoal = (e) => {
+    setInputValue(e.target.value);
+  };
 
+  const handleRemoveGoal = (e) => {
+    const id = parseInt(e.target.id);
+    setGoals(
+      goals.filter(goal =>
+        goal.id !== id
+      )
+    );
+  }
+  
   return (
     <>
-      <div>
+      <div className="container">
         <ul>
           {goals.map((goal, index) => 
-            <li key={index} className="">
-                {goal}
+            <li key={goal.id} className=" flex justify-between">
+              {goal.id}
+              {goal.text}
+              <button id={goal.id} onClick={handleRemoveGoal}>Supprimer</button>
             </li>
           )}
         </ul>
@@ -47,10 +65,10 @@ function App() {
         type="text"
         placeholder="Ajouter un nouveau but"
         value={inputValue}
-        onChange={e => setInputValue(e.target.value)}
+        onChange={handleInputGoal}
       />
       
-      <button onClick={handleGoals}>Ajouter</button>
+      <button onClick={handleAddGoal}>Ajouter</button>
     </>
   )
 }
