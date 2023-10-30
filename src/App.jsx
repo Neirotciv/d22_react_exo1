c3ccc318820eea81a0d70bc7c4aaf9eb2559f8cb
Neirotciv <tanacetifolia@gmail.com>
2023-10-30 14:34:09 +0100

    feat: show the list of goals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
 
-function App() {
+const sampleGoals = [
+  "Faire les courses",
+  "Aller à la salle de sport 3 fois par semaine",
+  "Monter à plus de 5000m d'altitude",
+  "Acheter mon premier appartement",
+  "Perdre 5 kgs",
+  "Gagner en productivité",
+  "Apprendre un nouveau langage",
+  "Faire une mission en freelance",
+  "Organiser un meetup autour de la tech",
+  "Faire un triathlon",
+];
 
+function App() {
   return (
     <>
-      
+      <ul>
+        {sampleGoals.map((goal, index) => 
+          <li key={index}>
+              {goal}
+          </li>
+        )}
+      </ul>
     </>
   )
 }
